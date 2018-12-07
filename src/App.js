396189c92308f4d1de3b595be50ec44b61dd58dc
h.likhtman <h.likhtman@gmail.com>
2018-12-07 19:06:19 +0200

    listView wip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import axios from 'axios'
 import SearchBar from './components/SearchBar'
 
 export default class App extends React.Component {
   onSearchSubmit = (search) => {
-    console.log(search)
+    axios.get('https://api.unsplash.com/search/photos?', {
+      headers: {
+        Authorization: 'Client-ID ',
+      },
+    }).then()
   }
 
   render() {
