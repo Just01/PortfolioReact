217654d455d187872ecbaa313f2c82970ec854a4
h.likhtman <h.likhtman@gmail.com>
2018-12-16 11:33:14 +0200

    react router dom intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import PostContainer from "./containers/postContainer/PostContainer"
+import PostContainer from './containers/postContainer/PostContainer'
+import { BrowserRouter, Route } from 'react-router-dom'
 
+const PageOne = () => {
+  return (
+    <div>
+      Page One! Do not use anchor tags!!!
+      <a href="/pagetwo">Navigate to Page Two!</a>
+    </div>
+  )
+}
+const PageTwo = () => {
+  return (
+    <div>
+      Page Two
+      <button>Click me!</button>
+      <a href="/">Navigate to Page One!</a>
+    </div>
+  )
+}
 export default class App extends React.Component {
   render() {
     return (
-      <div className="ui container">
-        <PostContainer />
+      <div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={PageOne}/>
+            <Route path="/pagetwo" exact component={PageTwo}/>
+          </div>
+        </BrowserRouter>
       </div>
     )
   }
